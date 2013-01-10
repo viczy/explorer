0eeeccd07a0036077893403e59aa0510599f213f
Lanvige Jiang <lanvige@gmail.com>
2013-01-10 13:34:40 +0800

    add regex kit and convert class

---
+++ ContentString.h
@@ -0,0 +1,15 @@
+//
+//  ContentString.h
+//  Explorer
+//
+//  Created by Lanvige Jiang on 1/10/13.
+//  Copyright (c) 2013 Lanvige Jiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContentString : NSObject
+
++ (NSString *)transformString:(NSString *)originalStr;
+
+@end
